4cb85a444b41728daafd43682f5c7e41f647cb86
IhorT <thekalgorsplay@gmail.com>
2021-11-09 20:19:25 +0200

    AdminPage: New Anime: implemented file uploading
    [Backup]

---
+++ API/Controllers/FileUploadController.cs
@@ -0,0 +1,50 @@
+﻿using AnimeLib.Domain.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeLib.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public string Post([FromForm] FileUpload objectFile)
+        {
+            try
+            {
+                if (objectFile.files.Length > 0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\Images\\";
+                    using (FileStream fs = System.IO.File.Create(path + objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fs);
+                        fs.Flush();
+                        return "Upload complete.";
+                    }
+                }
+                else
+                {
+                    return "Upload Error. No files given.";
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
